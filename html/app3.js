37f9f00d98ddd3509d496383cd4ef1fefc8aef87
djemalzecevic <djemal.zecevic@gmail.com>
2015-01-25 10:09:10 +0100

    Ajout liste avec fichier json
    Ajout liste dynamique avec fichier json pour être lu avec angulaires

--- html/app3.js
+++ html/app3.js
@@ -1,9 +1,18 @@
 (function() {
   var app = angular.module('gemStore', ['store-directives']);
 
-  app.controller('StoreController', function(){
-    this.products = gems;
-  });
+  app.controller('StoreController', ['$http', function($http){
+    //besoin de cette variable comme dans java
+    // instantie la variable store avec l'objet/module gemStore
+    var store = this;
+    //initialise la liste à vide 
+    store.products = [];
+    //recup donnée de json automatiquement transformer en liste par la fonctionne $http
+    // il existe d'autre avec $ devan comme $log etc.
+    $http('store-products.json').success(function(data){
+      store.products = data;
+    });
+  }]);
 
   app.controller('ReviewController', function() {
     this.review = {};
@@ -15,48 +24,4 @@
     };
   });
 
-  
-  var gems = [{
-      name: 'Azurite',
-      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-      shine: 8,
-      price: 110.50,
-      rarity: 7,
-      color: '#CCC',
-      faces: 14,
-      images: [
-        "../img/gem-02.gif",
-        "../img/gem-05.gif",
-        "../img/gem-09.gif"
-      ],
-      reviews: []
-    }, {
-      name: 'Bloodstone',
-      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-      shine: 9,
-      price: 22.90,
-      rarity: 6,
-      color: '#EEE',
-      faces: 12,
-      images: [
-        "../img/gem-01.gif",
-        "../img/gem-03.gif",
-        "../img/gem-04.gif",
-      ],
-      reviews: []
-    }, {
-      name: 'Zircon',
-      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-      shine: 70,
-      price: 1100,
-      rarity: 2,
-      color: '#000',
-      faces: 6,
-      images: [
-        "../img/gem-06.gif",
-        "../img/gem-07.gif",
-        "../img/gem-08.gif"
-      ],
-      reviews: []
-    }];
 })();
